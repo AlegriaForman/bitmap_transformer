083cd8b4ba968c985a9dfe47c1d1407b109c497c
AlegriaForman <alegriaforman@gmail.com>
2016-04-10 12:19:52 -0700

    revise gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,31 @@
 
 const gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var eslintRules = require('__dirname + .eslintrc');
-var paths = ['lib/*.js', 'test/*.js', 'index.js', 'gulpfile.js'];
+var eslintRules = require(__dirname + '/.eslintrc');
+var files = ['lib/*.js', '/index.js', '/gulpfile.js'];
 
-gulp.task('eslint', () => {
-  return gulp.src(paths)
+gulp.task('mocha:test', () => {
+  return gulp.src('./test/*.js')
+  .pipe(mocha());
+});
+
+gulp.task('lint:test', () => {
+  return gulp.src('./test/*.js')
+    .pipe(eslint(eslintRules))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:nontest', () => {
+  return gulp.src(files)
   .pipe(eslint(eslintRules))
   .pipe(eslint.format());
 });
+
+gulp.task('watch', () => {
+  gulp.watch('./test/*.js', ['lint:test', 'mochaTest']);
+  gulp.watch(files, ['lint:nontest', 'mochaTest']);
+});
+
+gulp.task('lint', ['lint:test', 'lint:nontest']);
+gulp.task('mochaTest', ['mocha:test']);
+gulp.task('default', ['lint', 'mochaTest']);
